e3a77b73c20b4134dd6117834a5d99b3e65e465d
jdonejdk <jdonejdk@protonmail.com>
2022-09-09 15:22:46 +0800

    fix error

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-common_package="neovim zsh openssh git curl shellcheck"
+common_package="zsh git curl shellcheck"
 
 function msg() {
   local text="$1"
@@ -17,27 +17,50 @@ function main() {
 }
 
 function ubuntu_init() {
-    local special_package="sudo build-essential silversearcher-ag pgp"
+  local special_package="sudo build-essential ssh"
 
-    echo 'Defaults env_reset,timestamp_timeout=-1' | sudo tee -a /etc/sudoers > /dev/null
+  msg "ubuntu init"
+  echo 'Defaults env_reset,timestamp_timeout=-1' | sudo tee -a /etc/sudoers > /dev/null
 
-    if [ 'grep -c "mirrors.tuna.tsinghua.edu.cn" "/etc/apt/sources.list"' -ne '0']; then
-      sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
-      sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
-      sudo apt update
-      sudo apt upgrade -y
-      sudo apt install -y ${common_package} ${special_package}
-      chsh -s $(which zsh)
-    fi
+  if [ "$(grep -c "mirrors.tuna.tsinghua.edu.cn" "/etc/apt/sources.list")" -eq '0' ]; then
+    sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+    sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+    echo '199.232.68.133 raw.githubusercontent.com' | sudo tee -a /etc/hosts > /dev/null
+    sudo apt update
+    sudo apt upgrade -y
+    sudo apt install -y "${common_package}" "${special_package}"
+    sudo systemctl enable ssh
+    sudo systemctl start ssh
+    sudo chsh -s "$(which zsh)" "$(whoami)"
+  fi
+  touch ~/.config/zsh/machine_specific.zsh
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+  if [ "$(grep -c "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git" "$HOME/.zprofile")" = '0' ]; then
+    msg "brew init"
+    git clone --depth=1 https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/install.git brew-install
+    NONINTERACTIVE=1 /bin/bash brew-install/install.sh
+    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.config/zsh/machine_specific.zsh
+    export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+    /usr/bin/zsh -c brew tap --custom-remote --force-auto-update homebrew/core https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
+    /usr/bin/zsh -c brew tap --custom-remote --force-auto-update homebrew/command-not-found https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-command-not-found.git
+    /usr/bin/zsh -c brew update
+    echo 'export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"' >> ~/.config/zsh/machine_specific.zsh
+    echo 'export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"' >> ~/.config/zsh/machine_specific.zsh
+  fi
+
 }
 
 function arch_init() {
-  local special_package="yay sudo nerd-fonts-fira-code nerd-fonts-victor-mono the_silver_searcher ccache base-devel lsd gnupg chezmoi fzf rust go"
+  local special_package="yay openssh sudo neovim nerd-fonts-fira-code nerd-fonts-victor-mono the_silver_searcher ccache base-devel lsd gnupg chezmoi fzf rust go kitty"
 
-   msg "arch init"
+  msg "arch init"
   echo 'Defaults env_reset,timestamp_timeout=-1' | sudo tee -a /etc/sudoers > /dev/null
 
-  if [ $(grep -c "mirrors.tuna.tsinghua.edu.cn" "/etc/pacman.conf") -eq '0' ]; then
+  if [ "$(grep -c "mirrors.tuna.tsinghua.edu.cn" "/etc/pacman.conf")" -eq '0' ]; then
     msg "add archlinuxcn"
     echo -e '[archlinuxcn]\nServer = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch' | sudo tee -a /etc/pacman.conf > /dev/null
     sudo pacman -Sy archlinuxcn-keyring --needed --noconfirm
@@ -50,18 +73,19 @@ function arch_init() {
     sudo sed -i '1i\Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch\n' /etc/pacman.d/mirrorlist
   fi
   sudo pacman -Syuu --needed --noconfirm
-  sudo pacman -Sy --needed --noconfirm ${common_package}
+  sudo pacman -Sy --needed --noconfirm "${common_package}"
   sudo systemctl enable sshd
-  sudo pacman -Sy --needed --noconfirm ${special_package}
+  sudo pacman -Sy --needed --noconfirm "${special_package}"
+  touch ~/.config/zsh/machine_specific.zsh
   echo -e "[user]\n\tname = jdonejdk\n\temail = jdonejdk@protonmail.com" > ~/.gitconfig
   chezmoi init https://github.com/jdonejdk/dotfiles.git
-  chezmoi apply 
-  chsh -s $(which zsh)
+  chezmoi apply
+  sudo chsh -s "$(which zsh)" "$(whoami)"
 
     # install sway desktop
     # sudo pacman -Sy --needed --noconfirm sway-git swaybg waybar
     # echo 'WLR_NO_HARDWARE_CURSORS=1' | sudo tee -a /etc/environment > /dev/null
-    # sudo gpasswd -a $(whoami) seat
+    # sudo gpasswd -a "$(whoami)" seat
     # sudo systemctl start seatd
     # sudo systemctl enable seatd
     # echo -e 'export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock\nexport XDG_RUNTIME_DIR=/run/user/$UID' | tee -a $HOME/.zshrc > /dev/null
